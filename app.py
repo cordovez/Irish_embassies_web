a9c410e0a5d55e4b313aeac80bf5cfd27b809d1d
Juan Carlos Cordovez-Mantilla <cordovez@el-cordovez.com>
2024-08-08 22:06:39 +0200

    Basic functionality

--- app.py
+++ app.py
@@ -1,45 +1,24 @@
-import pandas as pd
-from shiny.express import input, render, ui
-from queries.get import get_all
-from shinywidgets import render_widget
-import json
-import requests
-import os
-from ipyleaflet import Map, AwesomeIcon, Marker
-# from modules import map
+from shiny.express import render, ui, input
+from queries import get
 
-BASE_URL = 'http://localhost:8001'
-ui.panel_title("Irish Heads of Mission")
+# ------------------------------------------------------------------------
+# Define user interface
+# ------------------------------------------------------------------------
 
 
-@render_widget
-def diplomatic_map():
-    dfa = (53.33658, -6.25953)
-    center = (15.14, 18.81203)
-    world = Map(center=center, zoom=2)
-    star = AwesomeIcon(name="star", marker_color="green")
-    pointer = Marker(icon=star,
-                     location=dfa,
-                     dragable=False,
-                     title="Department of Foreign Affairs",
-                     alt="Dublin, Ireland",
-                     opacity=1)
-    # world.add(geo_json)
-    world.add(pointer)
-    return world
+with ui.sidebar(bg="#006845", id="main_sidebar"):
+    ui.input_selectize(id="sidebar_nav", label="Select an option",
+                       choices={"go to embassies": "Embassies",
+                                "go to representations": "Representations",
+                                "go to bilateral": "Bilateral Diplomacy ",
+                                "go to diplomats": "Diplomats",
+     })
 
-
-ui.input_text("text", "Search Diplomat", "first, last, or mission...")
-
-
-@render.data_frame
-async def heads_of_mission():
-    url = f'{BASE_URL}/diplomats'
-    dip_list = get_all(url)
-    df = pd.DataFrame(dip_list)
-    df["mission"] = df["mission"].str.title()
-
-    return df[["first_name", "last_name", "mission", "mission_type"]]
-
-
-
+with ui.layout_columns():
+    @render.data_frame
+    def data():
+        selection = input.sidebar_nav()
+        df = get.corresponding_df(selection)
+        if selection != "go to representations":
+            return render.DataGrid(df, filters=True, selection_mode="rows")
+        return render.DataGrid(df, selection_mode="rows")
--- queries/get.py
+++ queries/get.py
@@ -1,6 +1,58 @@
 import requests
+import pandas as pd
+
+BASE_URL = 'http://localhost:8001'
 
 
-def get_all(url):
-    response = requests.get(url)
-    return response.json()
+def corresponding_df(selection: str):
+    match selection:
+        case "go to representations":
+            return representations_df()
+        case "go to bilateral":
+            return bilateral_df()
+        case "go to diplomats":
+            return diplomats_df()
+        case _:
+            return embassies_df()
+
+
+def diplomats_df():
+    url = f'{BASE_URL}/public/diplomats'
+    df = _make_df(url)
+    selected = df[["last_name", "first_name", "mission_title"]]
+    return selected
+
+
+def representations_df():
+    url = f'{BASE_URL}/public/representations'
+    df = _make_df(url)
+    selected = df[["rep_name", "head_of_mission"]]
+    renamed = selected.rename(columns={"rep_name": "Representation",
+                                       "head_of_mission": "Ambassador"})
+    return renamed
+
+
+def embassies_df():
+    url = f'{BASE_URL}/public/embassies'
+    df = _make_df(url)
+    selected_cols = df[["country", "head_of_mission"]]
+    renamed = selected_cols.rename(columns={"country": "Country",
+                                            "head_of_mission": "Ambassador"})
+    return renamed
+
+
+def bilateral_df():
+    url = f'{BASE_URL}/public/countries'
+    df = _make_df(url)
+    bi_df = df.loc[df["accredited_to_ireland"] &
+                   df["hosts_irish_mission"]]
+    selected_cols = bi_df[["country_name", "with_mission_in"]]
+    renamed = selected_cols.rename(columns={"country_name": "Country",
+                                         "with_mission_in": "Embassy location"})
+    renamed["Embassy location"] = renamed["Embassy location"].str.title()
+    return renamed
+
+
+def _make_df(url):
+    data = requests.get(url).json()
+    return pd.DataFrame(data)
